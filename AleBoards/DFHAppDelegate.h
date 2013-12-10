3ca44c4bde9df1e44cb5d3439d0e1635f1864927
Greg Fiumara <greg@gregfiumara.com>
2013-12-10 08:31:58 -0500

    Initial add.

---
+++ AleBoards/DFHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DFHAppDelegate.h
+//  AleBoards
+//
+//  Created by Greg Fiumara on 12/10/13.
+//  Copyright (c) 2013 Greg Fiumara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AleBoards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AleBoards
+//
+//  Created by Greg Fiumara on 12/10/13.
+//  Copyright (c) 2013 Greg Fiumara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DFHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DFHAppDelegate class]));
+	}
+}
